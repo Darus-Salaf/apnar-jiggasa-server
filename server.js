b4656678766a0238b48c244871f217ddc96d34ed
rabius-sunny <rabiussunnyc3x@gmail.com>
2021-08-25 16:39:30 +0600

    insert many

--- server.js
+++ server.js
@@ -23,8 +23,9 @@ const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology:
 client.connect(err => {
     console.log(err ? err : 'Database Connected Successfully')
 
-    const postsCollection = client.db("facebook").collection("posts")
-    const videosCollection = client.db("facebook").collection("posts")
+    const postsCollection = client.db("apnarJiggasa").collection("posts")
+    const videosCollection = client.db("apnarJiggasa").collection("videos")
+    const questionsCollection = client.db("apnarJiggasa").collection("questions")
 
     // Get all posts for likhito-proshno, nari-ongon & nastikkobad
     app.get('/api/v1/posts/:cat/:subcat', (req, res) => {
@@ -35,24 +36,95 @@ client.connect(err => {
             .toArray((err, result) => res.send(result))
     })
 
-    // Get all videos corresponding to a specific category and subcategory
-    app.get('/api/v1/videos/:cat/:subcat', (req, res) => {
-        const cat = req.params.cat
-        const subcat = req.params.subcat
+    // Create a post corresponding to a specific category and subcategory
+    app.post('/api/v1/create/post', (req, res) => {
+        const post = req.body
 
-        videosCollection.find({ cat, subcat })
+        postsCollection.insertOne(post)
+            .then((err, result) => err ? res.send(err) : res.send(result.insertedCount))
+    })
+
+    // Create many post
+    app.post('/api/v1/create/posts', (req, res) => {
+        const post = req.body
+
+        postsCollection.insertMany(post)
+            .then((err, result) => err ? res.send(err) : res.send(result.insertedCount))
+    })
+
+    // Delete a post 
+    app.delete('/api/v1/delete/post/:id', (req, res) => {
+        postsCollection.deleteOne({ _id: ObjectId(req.params.id) })
+            .then((err, result) => res.send(result.deletedCount))
+    })
+
+    // Get all videos corresponding to a specific category
+    app.get('/api/v1/videos/:cat', (req, res) => {
+        const cat = req.params.cat
+
+        videosCollection.find({ cat })
             .toArray((err, result) => res.send(result))
     })
 
+    // Get videos corresponding to a specific Sheikh
+    app.get('/api/v1/videos/:sheikh', (req, res) => {
+        const sheikh = req.params.sheikh
 
+        videosCollection.find({ sheikh })
+            .toArray((err, result) => res.send(result))
+    })
 
+    // Create a video 
+    app.post('/api/v1/create/video', (req, res) => {
+        const video = req.body
 
+        videosCollection.insertOne(video)
+            .then((err, result) => err ? res.send(err) : res.send(result.insertedCount))
+    })
 
+    // Create many video 
+    app.post('/api/v1/create/video', (req, res) => {
+        const video = req.body
 
+        videosCollection.insertMany(video)
+            .then((err, result) => err ? res.send(err) : res.send(result.insertedCount))
+    })
 
+    // Delete a video 
+    app.delete('/api/v1/delete/video/:id', (req, res) => {
+        videosCollection.deleteOne({ _id: ObjectId(req.params.id) })
+            .then((err, result) => res.send(result.deletedCount))
+    })
 
+    // Create a question 
+    app.post('/api/v1/create/question', (req, res) => {
+        const question = req.body
 
+        questionsCollection.insertOne(question)
+            .then((err, result) => err ? res.send(err) : res.send(result.insertedCount))
+    })
 
+    // Get temporary question
+    app.get('/api/v1/questions/temp', (req, res) => {
+        const status = 'temp'
+
+        questionsCollection.find({ status })
+            .toArray((err, result) => res.send(result))
+    })
+
+    // Get temporary question
+    app.get('/api/v1/questions/per', (req, res) => {
+        const status = 'per'
+
+        questionsCollection.find({ status })
+            .toArray((err, result) => res.send(result))
+    })
+
+    // Delete a question 
+    app.delete('/api/v1/delete/question/:id', (req, res) => {
+        questionsCollection.deleteOne({ _id: ObjectId(req.params.id) })
+            .then((err, result) => res.send(result.deletedCount))
+    })
 
 
 })
